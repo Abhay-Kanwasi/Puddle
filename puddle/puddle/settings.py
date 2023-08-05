176bed8dbfccb133a0a19187645b172a126d9ab4
Yash Rasniya <67114982+yashrasniya@users.noreply.github.com>
2023-08-05 23:10:43 +0530

    Update settings.py

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nwel#g^htk_#r^+5-0++cp73r(k_at4c$5c=a%)jv8+og)x(!1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['43.205.208.104']
+ALLOWED_HOSTS = ['43.205.208.104','itgopeshwar.in']
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
