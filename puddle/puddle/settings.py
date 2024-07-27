75a481d32be590ebdd81c5e85c18d38fcf50ac03
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2024-07-27 14:48:53 +0530

    updated

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-nwel#g^htk_#r^+5-0++cp73r(k_at4c$5c=a%)jv8+og)x(!1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app']
 
 WSGI_APPLICATION = 'api.wsgi.application'
 
@@ -146,10 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
