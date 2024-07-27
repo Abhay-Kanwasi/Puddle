c3c7fa9d44d230b9acbcd696d7b9cd68a4f9d337
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2024-07-27 13:12:59 +0530

    Changes:
    (1) .gitignore and requirements.txt file added
    (2) debug is false now in settings.py

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nwel#g^htk_#r^+5-0++cp73r(k_at4c$5c=a%)jv8+og)x(!1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['43.205.208.104','itgopeshwar.in','abhay.itgopeshwar.in', '127.0.0.1']
 
